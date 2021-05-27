35fe01beed5be95f7e2fb0c6caa06adb2016347e
Vaughan Jones <vlj7491@gmail.com>
2021-05-27 20:37:21 +1000

    add tests and more code stuff

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from aws_lambda_powertools import Logger, Metrics
 from aws_lambda_powertools.logging import correlation_paths
+from aws_lambda_powertools.metrics import MetricUnit
 from aws_lambda_powertools.event_handler.api_gateway import ApiGatewayResolver, Response
-
+from datetime import datetime
 
 import boto3
 import json
@@ -11,6 +12,111 @@ app = ApiGatewayResolver()
 logger = Logger(service="github-webhook")
 metrics = Metrics(service="github-webhook", namespace="service")
 securityhub = boto3.client('securityhub')
+
+AWS_ACCOUNT_ID = os.environ.get('AWS_ACCOUNT_ID', '123456789')
+AWS_REGION = os.environ.get('AWS_REGION', 'ap-southeast-2')
+
+severity_levels = {
+  'low': 'LOW',
+  'moderate': 'MEDIUM',
+  'high': 'HIGH',
+  'critical': 'CRITICAL'  
+}
+
+def securityhub_severity_level(github_level: str):
+  values = {
+    'Low': 'LOW',
+    'Moderate': 'MEDIUM',
+    'High': 'HIGH',
+    'Critical': 'CRITICAL'
+  }
+
+  return values[github_level]
+
+def create_finding(payload):
+  repo_name = payload['repository']['name']
+  repo_owner = payload['repository']['owner']['login']
+  package_name = payload['alert']['affected_package_name']
+  cve_id = payload['alert']['external_identifier']
+  fixed_in = payload['alert']['fixed_in']
+  severity = payload['alert']['severity']
+  github_alert_id = payload['alert']['id']
+
+  logger.info("Creating finding", extra=payload)
+  metrics.add_metric(name="finding_created", unit=MetricUnit.Count, value=1)
+
+  resp = securityhub.batch_import_findings(
+    Findings=[
+      {
+        'SchemaVersion': '2018-10-08',
+        'AwsAccountId': AWS_ACCOUNT_ID,
+        'CreatedAt': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+        'UpdatedAt': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+        'Title': '%s affected by %s' % (package_name, cve_id),
+        'Description': '%s package %s affected by %s, fixed in %s' % (repo_name, package_name, cve_id, fixed_in),
+        'GeneratorId': 'github',
+        'Id': str(github_alert_id),
+        'ProductArn': 'arn:aws:securityhub:%s:%s:product/%s/default' % (AWS_REGION, AWS_ACCOUNT_ID, AWS_ACCOUNT_ID),
+        'Severity': {
+          'Label': severity_levels.get(severity)
+        },
+        'FindingProviderFields': {
+          'Severity': {
+            'Label': severity_levels.get(severity)
+          },
+          'Types': [
+            'Software and Configuration Checks/Vulnerabilities/CVE'
+          ]
+        },
+        'Resources': [
+          {
+            'Type': 'Other',
+            'Id': str(github_alert_id),
+            'Details': {
+              'Other': {
+                'github.com/repository.name': repo_name,
+                'github.com/repository.owner': repo_owner
+              }
+            }
+          }
+        ]
+      }
+    ]
+  )
+
+  logger.info(json.dumps(resp))
+  if resp['SuccessCount'] >= 1:
+    metrics.add_metric(name="findings_created",
+                       unit=MetricUnit.Count,
+                       value=resp['SuccessCount']
+    )
+
+    return { 
+      "message": "Successfully imported finding",
+      "statusCode": resp['ResponseMetadata']['HTTPStatusCode']
+    }
+  else:
+    metrics.add_metric(name="failed_creations",
+                       unit=MetricUnit.Count,
+                       value=resp['FailedCount']
+    )
+
+    return {
+      "message": "Failed to import finding",
+      "statusCode": resp['ResponseMetadata']['HTTPStatusCode']
+    }
+
+@app.post("/")
+def process():
+  action = app.current_event.json_body['action']
+  if action == 'create':
+    return create_finding(app.current_event.json_body)
+  else:
+    return Response(
+      status_code=422,
+      content_type="application/json",
+      body=json.dumps({"message": "Invalid event type"})
+    )
 
 @metrics.log_metrics
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
---
+++ app_test.py
@@ -0,0 +1,172 @@
+
+from dataclasses import dataclass
+from unittest.mock import call
+from botocore.stub import Stubber
+
+from app import securityhub
+
+import json
+import pytest
+import app
+
+@pytest.fixture
+def securityhub_stub():
+  securityhub_stub = Stubber(securityhub)
+  securityhub_stub.activate()
+  yield securityhub_stub
+  securityhub_stub.deactivate()
+
+@pytest.fixture
+def securityhub_successful_import():
+  return { "ResponseMetadata": {
+           "RequestId": "2cda4dc4-cca4-42e9-898b-e76efb4ba2f4",
+           "HTTPStatusCode": 200,
+           "HTTPHeaders": {
+             "date": "Thu, 27 May 2021 08:36:01 GMT",
+             "content-type": "application/json",
+             "content-length": "54",
+             "connection": "keep-alive",
+             "x-amzn-requestid": "2cda4dc4-cca4-42e9-898b-e76efb4ba2f4",
+             "x-amz-apigw-id": "f-r9rE7pywMFtqQ=",
+             "x-amzn-trace-id": "Root=1-60af59f1-40276be638c707c4296e5051"
+           },
+           "RetryAttempts": 0
+         },
+         "FailedCount": 0,
+         "SuccessCount": 1,
+         "FailedFindings": []
+       }
+
+
+@pytest.fixture
+def lambda_context():
+    @dataclass
+    class LambdaContext:
+        function_name: str = "test"
+        memory_limit_in_mb: int = 128
+        invoked_function_arn: str = "arn:aws:lambda:eu-west-1:809313241:function:test"
+        aws_request_id: str = "52fdfc07-2182-154f-163f-5f0f9a621d72"
+
+    return LambdaContext()
+
+@pytest.fixture
+def create_event():
+  return {
+    "action": "create",
+    "alert": {
+      "id": 576891938,
+      "affected_range": "< 4.3.1",
+      "affected_package_name": "bootstrap",
+      "external_reference": "https://nvd.nist.gov/vuln/detail/CVE-2019-8331",
+      "external_identifier": "CVE-2019-8331",
+      "ghsa_id": "GHSA-9v3m-8fp8-mj99",
+      "created_at": "2021-04-06T09:43:28Z",
+      "severity": "moderate",
+      "fixed_in": "4.3.1"
+    },
+    "repository": {
+      "id": 324689641,
+      "name": "nrl",
+      "full_name": "vlj91/nrl",
+      "owner": {
+        "login": "vlj91"
+      }
+    }
+  }
+
+@pytest.fixture
+def dismiss_event():
+  return {
+    "action": "dismiss",
+    "alert": {
+      "id": 576891938,
+      "affected_range": "< 4.3.1",
+      "affected_package_name": "bootstrap",
+      "external_reference": "https://nvd.nist.gov/vuln/detail/CVE-2019-8331",
+      "external_identifier": "CVE-2019-8331",
+      "ghsa_id": "GHSA-9v3m-8fp8-mj99",
+      "created_at": "2021-04-06T09:43:28Z",
+      "fixed_in": "4.3.1"
+    },
+    "repository": {
+      "id": 324689641,
+      "name": "nrl",
+      "full_name": "vlj91/nrl",
+    }
+  }
+
+@pytest.fixture
+def resolve_event():
+  return {
+    "action": "resolve",
+    "alert": {
+      "id": 576891938,
+      "affected_range": "< 4.3.1",
+      "affected_package_name": "bootstrap",
+      "external_reference": "https://nvd.nist.gov/vuln/detail/CVE-2019-8331",
+      "external_identifier": "CVE-2019-8331",
+      "ghsa_id": "GHSA-9v3m-8fp8-mj99",
+      "created_at": "2021-04-06T09:43:28Z",
+      "fixed_in": "4.3.1"
+    },
+    "repository": {
+      "id": 324689641,
+      "name": "nrl",
+      "full_name": "vlj91/nrl",
+    }
+  }
+
+@pytest.fixture
+def invalid_event():
+  return {
+    "action": "invalid",
+    "alert": {
+      "id": 576891938,
+      "affected_range": "< 4.3.1",
+      "affected_package_name": "bootstrap",
+      "external_reference": "https://nvd.nist.gov/vuln/detail/CVE-2019-8331",
+      "external_identifier": "CVE-2019-8331",
+      "ghsa_id": "GHSA-9v3m-8fp8-mj99",
+      "created_at": "2021-04-06T09:43:28Z",
+      "fixed_in": "4.3.1"
+    },
+    "repository": {
+      "id": 324689641,
+      "name": "nrl",
+      "full_name": "vlj91/nrl",
+    }
+  }
+
+def test_create_event(lambda_context, create_event, securityhub_successful_import, securityhub_stub):
+  securityhub_stub.add_response(
+    "batch_import_findings", securityhub_successful_import
+  )
+
+  event = {
+    "body": json.dumps(create_event),
+    "path": "/",
+    "httpMethod": "POST",
+    "requestContext": {
+      "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef"
+    }
+  }
+
+  resp = app.lambda_handler(event, lambda_context)
+  body = json.loads(resp['body'])
+
+  assert body['message'] == 'Successfully imported finding'
+  assert body['statusCode'] == 200
+  assert resp['headers']['Content-Type'] == 'application/json'
+
+def test_invalid_method(lambda_context):
+  event = {
+    "path": "/",
+    "httpMethod": "GET",
+    "requestContext": {
+      "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef"
+    }
+  }
+
+  resp = app.lambda_handler(event, lambda_context)
+  assert resp['statusCode'] == 404
+  assert resp['headers']['Content-Type'] == 'application/json'
