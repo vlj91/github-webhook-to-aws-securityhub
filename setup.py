e6907674f7449a5dcc3f099f03a4794500d8c85f
Vaughan Jones <vlj7491@gmail.com>
2021-05-29 11:44:13 +1000

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="github-webhook-to-securityhub",
+    version="0.0.1",
+    author="vlj91",
+    url="https://github.com/vlj91/github-webhook-to-securityhub",
+    packages=[
+        'app',
+    ],
+    python_requires=">=3.8"
+)
